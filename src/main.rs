e81f43c3dccd0123ded92239ad43eae09b8bf7bf
AramL <leandrojavr@gmail.com>
2016-02-05 11:39:40 -0300

    mos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#[derive(Default, Debug)]
+struct CPU{
+    A : u8,  // Accumulator
+    X : u8,  // Indexes
+    Y : u8,  
+    S : u8,  // Stack pointer
+    P : u8,  // Status
+    PC: u8,  // Program counter
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let c = CPU::default();
+    println!("CPU, {:?},", c);
 }
