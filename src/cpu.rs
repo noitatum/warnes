073c874a75a2fd201358a2adcbc43f8598984e78
AramL <leandrojavr@gmail.com>
2016-02-07 23:19:02 -0300

    sigh

--- src/cpu.rs
+++ src/cpu.rs
@@ -2,7 +2,6 @@ use std::fmt;
 use mem::Memory as Mem;
 use std::num::Wrapping as W;
 
-
 macro_rules! set_overflow {
     ($flags:expr) => ($flags = $flags | W(1 << 6));
 }
@@ -13,7 +12,7 @@ macro_rules! unset_overflow {
 
 macro_rules! uset_negative {
     ($flags:expr, $val:expr) => ( 
-        if ($val & (1 << 7)) == 0 {
+        if ($val & W(1 << 7)) == W(0) {
             $flags = $flags & !W(1 << 7)
         }else{
             $flags = $flags | W(1 << 7)
@@ -60,14 +59,14 @@ macro_rules! unset_carry {
     ($flags:expr) => ($flags = $flags & !W(1));
 }
 
-const OP_SPECIAL_TABLE : [fn(&mut CPU, &mut Mem) -> (); 4] = [
+const OP_SPECIAL_TABLE : [fn(&mut CPU, &mut Mem) -> u32; 4] = [
     CPU::brk,
-    CPU::invalid,
+    CPU::invalid_s,
     CPU::rti,
     CPU::rts,
 ];
 
-const OP_BRANCH_TABLE : [fn(&mut CPU, &mut Mem, i8) -> (); 8] = [
+const OP_BRANCH_TABLE : [fn(P : u8) -> bool; 8] = [
     CPU::bpl,
     CPU::bmi,
     CPU::bvc,
@@ -78,23 +77,23 @@ const OP_BRANCH_TABLE : [fn(&mut CPU, &mut Mem, i8) -> (); 8] = [
     CPU::beq,
 ];
 
-const OP_IMPLIED_TABLE : [fn(&mut CPU, &mut Mem) -> (); 32] = [
+const OP_IMPLIED_TABLE : [fn(&mut CPU, &mut Mem); 32] = [
     CPU::php,
     CPU::asl_a,
     CPU::clc,
-    CPU::invalid,
+    CPU::invalid_i,
     CPU::plp, 
     CPU::rol_a,
     CPU::sec,
-    CPU::invalid,
+    CPU::invalid_i,
     CPU::pha,
     CPU::lsr_a,
     CPU::cli,
-    CPU::invalid,
+    CPU::invalid_i,
     CPU::pla,
     CPU::ror_a,
     CPU::sei,
-    CPU::invalid,
+    CPU::invalid_i,
     CPU::dey,
     CPU::txa,
     CPU::tya,
@@ -106,11 +105,11 @@ const OP_IMPLIED_TABLE : [fn(&mut CPU, &mut Mem) -> (); 32] = [
     CPU::iny,
     CPU::dex,
     CPU::cld,
-    CPU::invalid,
+    CPU::invalid_i,
     CPU::inx,
     CPU::nop,
     CPU::sed,
-    CPU::invalid,
+    CPU::invalid_i,
 ];
 
 const OP_COMMON_TABLE : [fn(&mut CPU, &mut Mem, u8) -> (); 32] = [
@@ -148,26 +147,60 @@ const OP_COMMON_TABLE : [fn(&mut CPU, &mut Mem, u8) -> (); 32] = [
     CPU::invalid_c,
 ];
 
-const OP_JUMP_MASK     : u8 = 0xDF;
-const OP_JUMP          : u8 = 0x4C;
-const OP_SPECIAL_MASK  : u8 = 0x9F;
-const OP_SPECIAL       : u8 = 0x00;
-const OP_BRANCH_MASK   : u8 = 0x1F;
-const OP_BRANCH        : u8 = 0x10;
-const OP_IMPLIED_MASK  : u8 = 0x1F;
-const OP_IMPLIED       : u8 = 0x08;
-const OP_JSR           : u8 = 0x20;
+const OP_JUMP_MASK      : u8 = 0xDF;
+const OP_JUMP           : u8 = 0x4C;
+const OP_SPECIAL_MASK   : u8 = 0x9F;
+const OP_SPECIAL        : u8 = 0x00;
+const OP_BRANCH_MASK    : u8 = 0x1F;
+const OP_BRANCH         : u8 = 0x10;
+const OP_IMPLIED_MASK   : u8 = 0x1F;
+const OP_IMPLIED        : u8 = 0x08;
 
-const STACK_PAGE       : u16 = 0x0100;
+const OP_JSR            : u8 = 0x20;
+
+/* Implied instructions with more than two cycles */
+const OP_IMP_STACK_MASK : u8 = 0x9F;
+const OP_IMP_STACK      : u8 = 0x08;
+const OP_IMP_PULL_MASK  : u8 = 0xBF;
+const OP_IMP_PULL       : u8 = 0x28;
+
+const STACK_PAGE        : u16 = 0x0100;
+const PAGE_MASK         : u16 = 0xFF00;
+
+const FLAG_CARRY_MASK   : u8 = 0x01;
+const FLAG_ZERO_MASK    : u8 = 0x02;
+const FLAG_INT_MASK     : u8 = 0x04;
+const FLAG_DEC_MASK     : u8 = 0x08;
+const FLAG_BRK_MASK     : u8 = 0x10;
+const FLAG_UNUSED_MASK  : u8 = 0x20;
+const FLAG_OF_MASK      : u8 = 0x40;
+const FLAG_SIGN_MASK    : u8 = 0x80;
+
+/* Cycle cost */
+
+/* Jumps */
+const CYCLES_JUMP       : u32 = 3;
+const CYCLES_JSR        : u32 = 6;
+
+/* Branches */
+const CYCLES_BRANCH     : u32 = 2;
+
+/* Specials */
+const CYCLES_BRK        : u32 = 7;
+const CYCLES_RTI        : u32 = 6;
+const CYCLES_RTS        : u32 = 6;
+
+/* Implied */
+const CYCLES_IMPLIED    : u32 = 2;
 
 #[allow(non_snake_case)]
 pub struct CPU {
-    A : W<u8>,  // Accumulator
-    X : W<u8>,  // Indexes
-    Y : W<u8>,  
-    Flags : W<u8>,  // Status
-    SP: W<u8>,  // Stack pointer
-    PC: W<u16>, // Program counter
+    A       : W<u8>,  // Accumulator
+    X       : W<u8>,  // Indexes
+    Y       : W<u8>,  
+    Flags   : u8,  // Status
+    SP      : W<u8>,  // Stack pointer
+    PC      : W<u16>, // Program counter
 }
 
 fn load_word(memory: &mut Mem, address: W<u16>) -> u16 {
@@ -183,12 +216,12 @@ fn write_word(memory: &mut Mem, address: W<u16>, word: u16) {
 impl CPU {
     pub fn new() -> CPU {
         CPU {
-            A : W(0),
-            X : W(0),
-            Y : W(0),
-            Flags : W(0x24), 
-            SP : W(0xff),
-            PC : W(0),
+            A       : W(0),
+            X       : W(0),
+            Y       : W(0),
+            Flags   : 0x24, 
+            SP      : W(0xff),
+            PC      : W(0),
         }
     }
 
@@ -212,37 +245,62 @@ impl CPU {
         (self.pop(memory) as u16) << 8 | low
     }
 
-    pub fn execute(&mut self, memory: &mut Mem) {
-        let mut pc = self.PC;
-        let opcode = memory.load(pc.0);
-        pc = pc + W(1);
+    pub fn execute(&mut self, memory: &mut Mem) -> u32 {
+        let mut cycles : u32 = 0;
+        let opcode = memory.load(self.PC.0);
+
         if opcode & OP_JUMP_MASK == OP_JUMP {
             /* JMP */
-            let mut address = load_word(memory, pc); 
+            cycles = CYCLES_JUMP;
+            let mut address = load_word(memory, self.PC + W(1)); 
             if opcode & !OP_JUMP_MASK > 0 {
-                // Indirect Jump, +2 Cycles
+                // Indirect Jump, additional two cycles
+                cycles += 2;
                 address = load_word(memory, W(address));
             } 
-            self.jmp(memory, address);
+            self.PC = W(address);
         } else if opcode & OP_SPECIAL_MASK == OP_SPECIAL {
             /* Special */
-            if opcode == OP_JSR {
-                self.jsr(memory);
+            cycles = if opcode == OP_JSR {
+                // Load destination address and push return address
+                let pc = self.PC;
+                let address = load_word(memory, pc + W(1));
+                self.push_word(memory, (pc + W(3)).0);
+                self.PC = W(address);
+                CYCLES_JSR
             } else {
                 let index = (opcode >> 5) & 0x3;
-                OP_SPECIAL_TABLE[index as usize](self, memory);
+                OP_SPECIAL_TABLE[index as usize](self, memory)
             }
         } else if opcode & OP_BRANCH_MASK == OP_BRANCH {
             /* Branch */
-            let mut offset = memory.load(pc.0) as i8;
-            // To sign-magnitude
-            if offset < 0 { 
-                offset = -(offset & 0x7F);
-            }
+            cycles = CYCLES_BRANCH;
             let index = opcode >> 5;
-            OP_BRANCH_TABLE[index as usize](self, memory, offset);
+            if OP_BRANCH_TABLE[index as usize](self.Flags) {
+                // Additional cycle if branch taken
+                cycles += 1;
+                let pc = self.PC;
+                let mut offset = memory.load((pc + W(1)).0) as i8;
+                // To sign-magnitude
+                if offset < 0 { 
+                    offset = -(offset & 0x7F);
+                }
+                // Calculate branch address and push return address
+                let address = self.PC + W((offset as i16) as u16);
+                if (address & W(PAGE_MASK)) != (self.PC & W(PAGE_MASK)) {
+                    // Additional cycle if page boundary crossed
+                    cycles += 1;
+                }
+                self.push_word(memory, (pc + W(3)).0);
+                self.PC = address; 
+            }
         } else if opcode & OP_IMPLIED_MASK == OP_IMPLIED {
             /* Implied */
+            cycles = CYCLES_IMPLIED;
+            // Stack instructions get one additional cycle
+            cycles += (opcode & OP_IMP_STACK_MASK == OP_IMP_STACK) as u32; 
+            // An additional if it is a pull
+            cycles += (opcode & OP_IMP_PULL_MASK == OP_IMP_PULL) as u32;
             let index = ((opcode >> 4) & 0xE) + ((opcode >> 1) & 1);
             OP_IMPLIED_TABLE[index as usize](self, memory);
         } else { 
@@ -251,7 +309,47 @@ impl CPU {
             let index = ((opcode >> 3) & 0x1C) + (opcode & 0x3);
             OP_COMMON_TABLE[index as usize](self, memory, addressing);
         } 
+
+        return cycles;
     }
+}
+
+// Branch conditions
+
+impl CPU {
+
+    fn bpl (P: u8) -> bool {
+        P & FLAG_SIGN_MASK == 0
+    }
+
+    fn bmi (P: u8) -> bool {
+        P & FLAG_SIGN_MASK != 0
+    }
+
+    fn bvc (P: u8) -> bool {
+        P & FLAG_OF_MASK == 0
+    }
+
+    fn bvs (P: u8) -> bool {
+        P & FLAG_OF_MASK != 0
+    }
+
+    fn bcc (P: u8) -> bool {
+        P & FLAG_CARRY_MASK == 0
+    }
+
+    fn bcs (P: u8) -> bool {
+        P & FLAG_CARRY_MASK != 0
+    }
+
+    fn bne (P: u8) -> bool {
+        P & FLAG_ZERO_MASK == 0
+    }
+
+    fn beq (P: u8) -> bool {
+        P & FLAG_ZERO_MASK != 0
+    }
+
 }
 
 // Instructions
@@ -260,70 +358,31 @@ impl CPU {
 
     // Special
 
-    fn invalid(&mut self, memory: &mut Mem) -> () {
-
+    fn invalid_s(&mut self, memory: &mut Mem) -> u32 {
+        assert!(false);
+        return 0;
     }
 
-    fn brk(&mut self, memory: &mut Mem) -> () {
+    fn brk(&mut self, memory: &mut Mem) -> u32 {
+       
+       return CYCLES_BRK; 
+    }
+
+    fn rti(&mut self, memory: &mut Mem) -> u32 {
         
+       return CYCLES_RTI;
     }
 
-    fn rti(&mut self, memory: &mut Mem) -> () {
+    fn rts(&mut self, memory: &mut Mem) -> u32 {
         
+       return CYCLES_RTS;
     }
 
-    fn rts(&mut self, memory: &mut Mem) -> () {
-        
-    }
-
-    // Jumps
-
-    fn jmp(&mut self, memory: &mut Mem, address: u16) {
-        
-    }
-
-    fn jsr(&mut self, memory: &mut Mem) {
-        let pc = self.PC + W(1);
-        let address = load_word(memory, pc);
-        self.push_word(memory, (pc + W(2)).0);
-        self.PC = W(address);
-    }
-
-    // Branches
-
-    fn bpl (&mut self, memory: &mut Mem, offset: i8) {
-
-    }
-
-    fn bmi (&mut self, memory: &mut Mem, offset: i8) {
-
-    }
-
-    fn bvc (&mut self, memory: &mut Mem, offset: i8) {
-
-    }
-
-    fn bvs (&mut self, memory: &mut Mem, offset: i8) {
-
-    }
-
-    fn bcc (&mut self, memory: &mut Mem, offset: i8) {
-
-    }
-
-    fn bcs (&mut self, memory: &mut Mem, offset: i8) {
-
-    }
-
-    fn bne (&mut self, memory: &mut Mem, offset: i8) {
-
-    }
-
-    fn beq (&mut self, memory: &mut Mem, offset: i8) {
-
-    }
-    
     // Implied
+
+    fn invalid_i(&mut self, memory: &mut Mem) { 
+        assert!(false);
+    }
 
     fn php (&mut self, memory: &mut Mem) {
 
@@ -407,7 +466,7 @@ impl CPU {
 
     fn iny (&mut self, memory: &mut Mem) {
         self.Y = self.Y + W(1);
-        if self.Y == 0{
+        if self.Y == W(0){
             set_zero!(self.Flags);
         }else{
             unset_zero!(self.Flags);
@@ -425,7 +484,7 @@ impl CPU {
 
     fn inx (&mut self, memory: &mut Mem) {
         self.X = self.X + W(1);
-        if self.X == 0{
+        if self.X == W(0){
             set_zero!(self.Flags);
         }else{
             unset_zero!(self.Flags);
@@ -443,8 +502,8 @@ impl CPU {
 
     // Common
 
-    fn invalid_c(&mut self, memory: &mut Mem, addressing: u8) -> () {
-
+    fn invalid_c(&mut self, memory: &mut Mem, addressing: u8) {
+        assert!(false);
     }
 
     fn ora (&mut self, memory: &mut Mem, addressing: u8) {
@@ -484,13 +543,7 @@ impl CPU {
     }
 
     fn sty (&mut self, memory: &mut Mem, addressing: u8) {
-        /*let add_res : W<u8> = self.X + W(1);
-        
-        if (self.X ^ add_res) & (W(1) ^ add_res) & W(0x80) > W(1) {
-            set_overflow!(self.Flags);
-        }else{
-            unset_overflow!(self.Flags);
-        }*/
+
     }
 
     fn sta (&mut self, memory: &mut Mem, addressing: u8) {
@@ -541,8 +594,6 @@ impl CPU {
 impl fmt::Display for CPU {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{{ A: {}, X: {}, Y: {}, P: {}, SP: {}, PC: {} }}",
-               self.A.0 , self.X.0 , self.Y.0 , self.Flags.0 , self.SP.0 , self.PC.0)
+               self.A.0 , self.X.0 , self.Y.0 , self.Flags , self.SP.0 , self.PC.0)
     }
 }
-
-
