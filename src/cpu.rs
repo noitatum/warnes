d1ff940c676cbe5b095c8c40f9421063a20a5da7
AramL <leandrojavr@gmail.com>
2016-02-08 21:13:12 -0300

    macros.rs

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+use macros;
 use std::fmt;
 use mem::Memory as Mem;
 use std::num::Wrapping as W;
@@ -10,43 +12,6 @@ const FLAG_BRK          : u8 = 0x10;
 const FLAG_UNUSED       : u8 = 0x20;
 const FLAG_OVERFLOW     : u8 = 0x40;
 const FLAG_SIGN         : u8 = 0x80;
-
-macro_rules! set_flag {
-    ($flags:expr, $val:expr) => ($flags |= $val);
-}
-
-macro_rules! unset_flag {
-    ($flags:expr, $val:expr) => ($flags &= !$val);
-}
-
-macro_rules! is_flag_set {
-    ($flags:expr, $val:expr) => ($flags & $val != 0);
-}
-
-macro_rules! set_sign {
-    ($flags:expr, $val:expr) => ( 
-        $flags = $flags & !FLAG_SIGN | $val & FLAG_SIGN;
-    );
-}
-
-macro_rules! set_zero {
-    ($flags:expr, $val:expr) => (
-        set_flag!($flags, (($val == 0) as u8) << 1);
-    );
-}
-
-
-macro_rules! ror {
-    ($val:expr, $flags:expr) => ( $val = ($val >> 1) | (($val & W(get_bit!($flags, FLAG_CARRY))) << 7));
-}
-
-macro_rules! rol {
-    ($val:expr, $flags:expr) => ($val = ($val << 1) | (($val & W(get_bit!($flags, FLAG_CARRY))) >> 7));
-}
-
-macro_rules! get_bit {
-    ($flags:expr, $flag_bit:expr) => ($flags & $flag_bit;);
-}
 
 const OP_SPECIAL_TABLE : [fn(&mut CPU, &mut Mem) -> u32; 4] = [
     CPU::brk,
