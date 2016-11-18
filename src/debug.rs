7716d4d22b5fcb849fec52e5c92941fca65d02c7
NoiTaTuM <noit63@gmail.com>
2016-11-18 17:45:55 -0300

    Fix debug formatting

--- src/debug.rs
+++ src/debug.rs
@@ -3,16 +3,12 @@ use nes::Nes;
 use cpu::Operation;
 // std
 use std::io;
-
-macro_rules! rdbg {
-    () => { "(rdbg)" }
-}
+use std::io::Write;
 
 // Macro to print undefined commands. It removes the newline.
 macro_rules! undefinedc {
     ($input:expr) => (
-        println!("{} Undefined command: {}. Try 'help'", rdbg!(),
-                        $input.trim());
+        println!("Undefined command: {}. Try 'help'", $input.trim());
     );
 }
 
@@ -24,12 +20,11 @@ const DEBUG_LIST_SIZE   : u32 = 5;
 
 pub fn run(nes: &mut Nes) {
     nes.reset();
-    let mut input : String;
-    let stdin = io::stdin();
     'debug: loop {
-        print!("{} ", rdbg!());
-        input = String::new();
-        stdin.read_line(&mut input).unwrap();
+        let mut input = String::new();
+        print!("(rdbg) ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut input).unwrap();
         let words : Vec<&str> = input.split(" ").collect();
         if words.len() == 0 {
             continue;
@@ -49,28 +44,27 @@ pub fn run(nes: &mut Nes) {
                 step_cycle(nes);
             },
             "c"|"continue" => {
-                println!("{} continue", rdbg!());
+                println!("continue");
                 // TODO: nes.run();
                 break 'debug;
             },
             "p" => {
                 if words.len() == 1 {
-                    undefinedc!("No register or memory position given");
+                    println!("No register or memory position given");
                 } else {
                     print_reg(nes, words[1]);
                 }
             },
             "pb" => {
-                println!("{} print", rdbg!());
+                println!("print");
                 if words.len() == 1 {
-                    undefinedc!("No register or memory position given");
+                    println!("No register or memory position given");
                 } else {
                     print_reg_binary(nes, words[1]);
                 }
             },
-            "b" => println!("{} breakpoint", rdbg!()),
+            "b" => println!("breakpoint"),
             "q"|"quit" => {
-                print!("{} ", rdbg!());
                 break 'debug;
             },
             "help"  => help(),
@@ -142,11 +136,11 @@ fn print_list(nes: &mut Nes, count: u32) {
 */
 
 fn print_reg(nes: &Nes, word: &str) {
-    println!("{} {}: {:x}", rdbg!(), word, get_reg(nes, word));
+    println!("{}: {:x}", word.trim(), get_reg(nes, word));
 }
 
 fn print_reg_binary(nes: &Nes, word: &str) {
-    println!("{} {}: {:b}", rdbg!(), word, get_reg(nes, word));
+    println!("{}: {:b}", word.trim(), get_reg(nes, word));
 }
 
 fn get_reg(nes: &Nes, word: &str) -> u16 {
@@ -157,17 +151,17 @@ fn get_reg(nes: &Nes, word: &str) -> u16 {
         "P"     => nes.cpu().registers().P.0 as u16,
         "SP"    => nes.cpu().registers().SP.0 as u16,
         "PC"    => nes.cpu().registers().PC.0,
-        _       => {println!("{} Error non register returning 0", rdbg!()); 0},
+        _       => {println!("Error non register returning 0"); 0},
     }
 }
 
 fn help() {
-    print!("{} Help commands\n", rdbg!());
-    print!("{} 'c' or 'continue' to continue the execution.\n", rdbg!());
-    print!("{} 'n', 'next', 'step' or 'step' to do execute the next instruction or do a single cpu cycle.\n", rdbg!());
-    print!("{} 'b' or 'breakpoint' for breakpoints (NOT IMPLEMENTED YET).\n", rdbg!());
-    print!("{} 'q' or 'quit' to quit.\n", rdbg!());
-    print!("{} 'l' or 'list' to show the next instructions to be executed", rdbg!());
-    print!("{} 'p' plus a register name to show the value of the register (ex: p A).\n", rdbg!());
-    print!("{} 'pb' to show that value in binary (ex: pb A).\n", rdbg!());
+    println!("Help commands");
+    println!("'c' or 'continue' to continue the execution.");
+    println!("'n', 'next', 'step' or 'step' to do execute the next instruction or do a single cpu cycle.");
+    println!("'b' or 'breakpoint' for breakpoints (NOT IMPLEMENTED YET).");
+    println!("'q' or 'quit' to quit.");
+    println!("'l' or 'list' to show the next instructions to be executed");
+    println!("'p' plus a register name to show the value of the register (ex: p A).");
+    println!("'pb' to show that value in binary (ex: pb A).");
 }
